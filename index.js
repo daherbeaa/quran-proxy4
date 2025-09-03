a1f500ac7ba4e994802f8579e13095a4b7c6fd83
daherbeaa <65828706+daherbeaa@users.noreply.github.com>
2025-09-04 00:18:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const url = "https://live.eu-north-1b.cf.dmcdn.net/sec2(XXXX)/dm/3/x84wyku/s/live-480.m3u8"; // حط هنا لينك البث الجديد
+  const url = "https://live.eu-north-1b.cf.dmcdn.net/sec2(P46OQlQ0X_s8ZOnoKrBzN0JUHsqtpb-0xuvfvlpRL8eKM9_i8rIDRnrKccUfIgLLq3eDJPWDtm9BKV31pRV0DzFBzOjQhGvwGT0wKsKRXlo21KC-039YPc0Ij2MBYMQu)/dm/3/x84wyku/s/live-480.m3u8";
 
   try {
     const response = await fetch(url, {
